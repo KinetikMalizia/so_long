07b84d9bd6446e56a29f3e4699187309061ddcb8
JeanCarlen <94684234+JeanCarlen@users.noreply.github.com>
2022-03-03 16:38:04 +0100

    solong niquel

---
+++ So_long/check.c
@@ -0,0 +1,94 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jeancarlen <jeancarlen@student.42.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/03 13:42:12 by jeancarlen        #+#    #+#             */
+/*   Updated: 2022/02/03 13:59:57 by jeancarlen       ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "so_long.h"
+
+void	map_length(t_data *data)
+{
+	if (data->size_y > MAX_Y || data->size_x > MAX_X)
+	prline("/!\\ map is too big! /!\\ \n", 0);
+}
+
+void	check_player_exit(t_data *data)
+{
+	if (data->map.play != 1)
+	{
+		prline("Error\nMap must contain only 1 player!", -1);
+		exit(EXIT_FAILURE);
+	}
+	if (data->map.collectables == 0)
+	{
+		prline("Error\nno collectables on map!", -1);
+		exit(EXIT_FAILURE);
+	}
+	if (data->map.exit == 0)
+	{
+		prline("Error\nMap must have an exit\n", -1);
+		exit(EXIT_FAILURE);
+	}
+}
+
+void	ft_window_size(t_data *data, char **argv)
+{
+	int		fd;
+	char	*word;
+
+	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+	{
+		prline("Error\nInvalid map_path/map\n", 0);
+		exit(EXIT_FAILURE);
+	}
+	word = ft_strnstr(argv[1], ".ber", ft_strlen(argv[1]));
+	if (word == NULL || word[4] != '\0')
+	{
+		prline("Error\nmap has to be .ber", 0);
+		exit(EXIT_FAILURE);
+	}
+	data->size_x = (ft_line_length(fd) * IMG_W);
+	data->size_y = (ft_count_lines(fd, data->size_x, IMG_W) * IMG_H);
+}
+
+void	wall_check(t_data *data)
+{
+	int	x;
+	int	y;
+
+	x = 0;
+	while (x < data->size_x / IMG_W)
+	{
+		y = 0;
+		while (y < data->size_y / IMG_H)
+		{
+			if ((y == 0 || x == 0) && data->map.map[y][x] != '1')
+			{
+				prline("manque de wall", -1);
+				exit(EXIT_FAILURE);
+			}
+			if ((x == (data->size_x / IMG_H - 1)
+					|| y == (data->size_y / IMG_W - 1))
+				&& data->map.map[y][x] != '1')
+			{
+				prline("manque de wall", -1);
+				exit(EXIT_FAILURE);
+			}
+			y++;
+		}
+		x++;
+	}
+}
+
+void	exit_error(void)
+{
+	prline("Error\nwrong map dimensions", 0);
+	exit(EXIT_FAILURE);
+}
---
+++ So_long/so_long.h
@@ -0,0 +1,176 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   so_long.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: arvnrlen <arvenn@student.42.fr>          +#+  +:+       +#+          */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/01 12:19:57 by fmalizia          #+#    #+#             */
+/*   Updated: 2022/02/03 11:14:05 by arvn         ###   ########.fr     	  */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SO_LONG_H
+# define SO_LONG_H
+
+# include "mlx/mlx.h"
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+# define IMG_W 64
+# define IMG_H 64
+# define ESC 53
+# define W 6
+# define A 12
+# define S 1
+# define D 2
+# define UP 126
+# define DOWN 125
+# define LEFT 123
+# define RIGHT 124
+# define MAX_X 1080
+# define MAX_Y 720
+
+typedef struct s_wall
+{
+	void	*wall1;
+	void	*wall2;
+	void	*wall3;
+	void	*wall4;
+}				t_wall;
+
+typedef struct s_img
+{
+	t_wall		*wall;
+	void		*ground;
+	void		*player;
+	void		*collectable;
+	void		*exit_s;
+	void		*exit_w;
+}				t_img;
+
+typedef struct s_player
+{
+	int	p_x;
+	int	p_y;
+	int	delta_x;
+	int	delta_y;
+}				t_player;
+
+typedef struct s_map
+{
+	char	**map;
+	int		x;
+	int		y;
+	int		collectables;
+	int		play;
+	int		exit;
+}				t_map;
+
+typedef struct s_data {
+	void		*mlx;
+	void		*win;
+	int			size_x;
+	int			size_y;
+	int			steps;
+	int			wall_flag;
+	t_map		map;
+	t_img		img;
+	t_player	player;
+}				t_data;
+
+int		win_exit(int keycode, t_data *vars);
+int		ft_line_length(int fd);
+int		ft_count_lines(int fd, int x, int img_w);
+char	*ft_strnstr(const char *s1, const char *s2, unsigned long n);
+char	*ft_strjoin_malloc(char *s1, char *buff);
+size_t	ft_strlen(const char *s);
+int		keyhook_inputs(int keycode, t_data *vars);
+void	ft_window_size(t_data *data, char **argv);
+void	*ft_calloc(size_t c, size_t n);
+void	ft_bzero(void *b, size_t len);
+void	init_map(char *path, t_data *data);
+void	rd_map(int fd, t_data *data);
+int		create_back(t_data *data);
+void	put_object(t_data *data);
+void	wall_check(t_data *data);
+void	setup_img(t_data *data);
+void	setup_player(t_data *data);
+int		mvt_check(t_data *data);
+int		mvt_check_1(t_data *data, int new_x, int new_y);
+void	prline(char *str, int steps);
+void	win_game(t_data *data);
+void	next_frame(t_data *data);
+void	put_player(t_data *data);
+void	collect(t_data *data, int new_x, int new_y);
+void	print_map(t_data *data);
+void	print_objects(t_data *data, int x, int y);
+void	print_map_2(t_data *data, int x, int y);
+void	setup_wall(t_data *data);
+void	turn_player(t_data *data, int type, int w, int h);
+void	print_wall(t_data *data, int x, int y);
+void	check_player_exit(t_data *data);
+void	map_length(t_data *data);
+void	exit_error(void);
+int		key_hook(t_data *data);
+
+#endif
+
+/*
+
+check
+	map_length /
+	check_player_exit
+	ft_window_size
+	wall_check
+input
+	keyhook_inputs
+
+map_init
+	ft_count_lines
+	ft_line_length
+	ft_strlen
+	init_map
+	rd_map
+	****************
+
+setup_memory
+	setup_wall
+	setup_img
+	setup_player
+
+run_prog
+	collect
+	put_player
+	next_frame
+	mvt_check
+	mvt_check_1
+
+print
+	print_wall
+	turn_player
+	print_map
+	print_map_2
+	*************
+	create_back
+	put_object
+	print_objects
+
+so_long
+	main
+	win_exit
+	win_game
+
+so_long_utils
+	ft_strnstr
+	ft_strjoin_malloc
+	ft_strlen
+	ft_calloc
+	ft_bzero
+
+print_text
+	prline
+	ft_putstr_fd
+*/
